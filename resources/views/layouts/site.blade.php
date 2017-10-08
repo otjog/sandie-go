1cdef5f6566ff8ec2184b4c6141f3adbc3aca380
otjog <fvirus@mail.ru>
2017-10-08 13:01:55 -0700

    Added Ya.Metrika

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -36,6 +36,9 @@
             </p>
         </div>
     </footer>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter46207134 = new Ya.Metrika({ id:46207134, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/46207134" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 
 <script type="text/javascript" src="{{asset('assets/js/jquery-3.2.1.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/bootstrap-datepicker.min.js')}}"></script>
