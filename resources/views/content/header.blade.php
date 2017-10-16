7d6f5fb3a8e4001aba0e3948f03ea501b888fbf7
otjog <fvirus@mail.ru>
2017-10-16 00:08:27 -0700

    Added link for route on header

--- resources/views/content/header.blade.php
+++ resources/views/content/header.blade.php
@@ -5,6 +5,14 @@
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Выбрать направление <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{{route('order', ['alias' => 'from_la_to_sd'])}}">из Аэропорта Лос-Анджелес в Сан Диего</a></li>
+                        <li class="divider"></li>
+                        <li><a href="{{route('order', ['alias' => 'from_sd_to_la'])}}">из Сан Диего в Аэропорт Лос-Анджелеса</a></li>
+                    </ul>
+                </li>
                 <li><a href="{{route('contact')}}">Написать нам</a></li>
             </ul>
         </div>
--- resources/views/content/order.blade.php
+++ resources/views/content/order.blade.php
@@ -3,7 +3,8 @@
         <div class="row">
             <div class="panel panel-primary">
                 <div class="panel-heading">
-                    <h1 class="text-center" style="color: white">Оформление поездки <small style="color: white">{{$direction->name}}</small></h1>
+                    <h2 class="text-center" style="color: white">Оформление поездки</h2>
+                    <h3 class="text-center" style="color: white">{{$direction->name}}</h3>
                 </div>
                 <div class="panel-body">
                     <div class="row">
